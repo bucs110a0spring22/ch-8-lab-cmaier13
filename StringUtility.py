21c1c4394f84244b6bed18d1638b74fe5f864bf7
Caitlyn Maier <98415962+cmaier13@users.noreply.github.com>
2022-04-11 18:50:56 +0000

    part of vowels

--- StringUtility.py
+++ StringUtility.py
@@ -6,6 +6,14 @@ class StringUtility:
   def __str__(self):
     return self.string
 
+  def vowels(self):
+    count = 0
+    mystring = self.string
+    vowels = 'aeiouAEIOU'
+    for alphabet in mystring:
+      if alphabet in vowels:
+        count = count + 1
+    return count
     
       
       
